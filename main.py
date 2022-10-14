1d1f524601f818cd2e056ba60abacdf49ed421d9
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-14 03:06:08 +0000

    correct order, looks completed, check later

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Math = [100, 90]
 English = [90, 80]
 Physics = [80, 80]
 Computer = [80, 90]
-x = np.arange(2) 
+x = np.arange(2)
 width = 0.15
 labels = ['Math', 'English', 'Physics', 'Computer']
 names = ['Bill', 'Mary']
@@ -13,9 +13,11 @@ names = ['Bill', 'Mary']
 # ******************************
 # Make your code
 # ******************************
-# using dist from edges and subject number index
-def makebarm(num, subj): 
-    return ax.bar(x - width*num, datab[subj][1:], width, label = datab[subj][0])
+# 
+fig, ax = plt.subplots()
+
+def makebarm(num, subj, subname):  # number from center, subject list, label of the subject from labels list
+    return ax.bar(x - width*num, subj, width, label = subname)
 
 # copy of function for making bar right of center to go right, unsure of parsing so unfortunately make a new one with just - -> +
 def makebarp(num, subj, subname): 
@@ -28,8 +30,7 @@ s_phy = makebarp(.5, Physics, labels[2])
 s_comp = makebarp(1.5, Computer, labels[3])
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Scores')
-ax.set_title(f'Scores of {names[0]} and {names[1]}')
+ax.set_title('Grouped Graph for Scores')
 ax.set_xticks(x, names)
 ax.legend()
 
